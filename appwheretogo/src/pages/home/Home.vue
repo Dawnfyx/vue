21f46f3583e548ee5e612bb7da7ad9f17606ab78
Dawn <15021677496@163.com>
2019-07-30 17:03:36 +0800

    recommend

--- appwheretogo/src/pages/home/Home.vue
+++ appwheretogo/src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
     <div>test</div>
   </div>
 </template>
@@ -11,13 +12,15 @@
   import HomeHeader from './components/Header'
   import HomeSwiper from './components/Swiper'
   import HomeIcons from './components/Icons'
+  import HomeRecommend from './components/Recommend'
 
   export default {
     name: 'Home.vue',
     components: {
       HomeHeader: HomeHeader,
       HomeSwiper: HomeSwiper,
-      HomeIcons: HomeIcons
+      HomeIcons: HomeIcons,
+      HomeRecommend: HomeRecommend
     }
   }
 </script>
