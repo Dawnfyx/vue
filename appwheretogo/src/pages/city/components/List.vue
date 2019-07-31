bb1672774e35474e3a0804e6d9827a4e9b9ba6c5
Dawn <15021677496@163.com>
2019-07-31 12:09:58 +0800

    alphabet

--- appwheretogo/src/pages/city/components/List.vue
+++ appwheretogo/src/pages/city/components/List.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="list">
+  <div class="list" ref="wrapper">
     <div>
       <div class="area">
         <div class="title border-topbottom">当前城市</div>
@@ -44,11 +44,58 @@
           <div class="item border-bottom">
             ssss
           </div>
+          <div class="item border-bottom">
+            ssss
+          </div>
+          <div class="item border-bottom">
+            ssss
+          </div>
+          <div class="item border-bottom">
+            ssss
+          </div>
+          <div class="item border-bottom">
+            ssss
+          </div>
         </div>
       </div>
       <div class="area">
         <div class="title border-topbottom">B</div>
         <div class="item-list">
+          <div class="item border-bottom">
+            ssss
+          </div>
+          <div class="item border-bottom">
+            ssss
+          </div>
+          <div class="item border-bottom">
+            ssss
+          </div>
+          <div class="item border-bottom">
+            ssss
+          </div>
+          <div class="item border-bottom">
+            ssss
+          </div>
+          <div class="item border-bottom">
+            ssss
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">C</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            ssss
+          </div>
+          <div class="item border-bottom">
+            ssss
+          </div>
+          <div class="item border-bottom">
+            ssss
+          </div>
+          <div class="item border-bottom">
+            ssss
+          </div>
           <div class="item border-bottom">
             ssss
           </div>
@@ -62,8 +109,13 @@
 </template>
 
 <script>
+  import Bscroll from 'better-scroll'
+
   export default {
-    name: 'CityList'
+    name: 'CityList',
+    mounted () {
+      this.scroll = new Bscroll(this.$refs.wrapper)
+    }
   }
 </script>
 
