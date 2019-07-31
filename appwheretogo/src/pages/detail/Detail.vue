e1887bdc06cf3c36f6b5ddaf9d1e235f243bdd8a
Dawn <15021677496@163.com>
2019-07-31 22:38:15 +0800

    scrollTop opacity

--- appwheretogo/src/pages/detail/Detail.vue
+++ appwheretogo/src/pages/detail/Detail.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
   </div>
 </template>
 
 <script>
   import DetailBanner from "./components/Banner"
+  import DetailHeader from './components/Header'
+  // import DetailList from './components/List'
 
   export default {
     name: 'Detail',
     components: {
-      DetailBanner: DetailBanner
+      DetailBanner: DetailBanner,
+      DetailHeader: DetailHeader
     }
   }
 </script>
