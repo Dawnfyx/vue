6970411a48b77a0e20b886d7e2c9effcf7ed110f
Dawn <15021677496@163.com>
2019-07-31 15:26:26 +0800

    search 优化

--- appwheretogo/src/pages/city/components/Search.vue
+++ appwheretogo/src/pages/city/components/Search.vue
@@ -1,19 +1,78 @@
 <template>
   <div>
     <div class="search">
-      <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
     </div>
-    <div>
+    <div class="search-content"
+         ref="search"
+         v-show="keyword">
       <ul>
-
+        <li
+          class="search-item border-bottom"
+          v-for="item of list"
+          :key="item.id"
+          @click="handleCityClick(item.name)"
+        >{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">
+          没有找到匹配数据
+        </li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+  import Bscroll from 'better-scroll'
+
   export default {
-    name: 'Search'
+    name: 'CitySearch',
+    props: {
+      cities: Object
+    },
+    data: function () {
+      return{
+        keyword: "",
+        list: [],
+        timer: null
+      }
+    },
+    computed: {
+      hasNoData () {
+        return !this.list.length
+      }
+    },
+    watch: {
+      keyword (){
+        if(this.timer){
+          clearTimeout(this.timer)
+        }
+        if (!this.keyword) {
+          this.list = []
+          return
+        }
+        this.timer = setTimeout(() => {
+          const result = []
+          for (let i in this.cities) {
+            // 搜索过滤操作
+            this.cities[i].forEach((value) => {
+              // value.spell 城市的拼音  value.name 城市的名字
+              if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+                result.push(value);
+              }
+            })
+          }
+          this.list = result
+        }, 100)
+      }
+    },
+    methods: {
+      handleCityClick (city){
+        console.log("handleCityClick", city);
+      }
+    },
+    mounted () {
+      this.scroll = new Bscroll(this.$refs.search)
+    }
   }
 </script>
 
