447a8bf92ea87630f5e1f65eab187ca1c4bc2a7f
Dawn <15021677496@163.com>
2019-07-31 14:02:24 +0800

    alphabet touch优化

--- appwheretogo/src/pages/city/components/Alphabet.vue
+++ appwheretogo/src/pages/city/components/Alphabet.vue
@@ -19,7 +19,9 @@
     },
     data:  function(){
       return{
-        touchStatus: false
+        touchStatus: false,
+        startY: 0,
+        timer: null
       }
     },
     computed: {
@@ -31,6 +33,9 @@
         return letters
       }
     },
+    updated () {
+      this.startY = this.$refs['A'][0].offsetTop
+    },
     methods: {
       handleLetterClick (e){
         this.$emit("change", e.target.innerText);
@@ -39,13 +44,18 @@
         this.touchStatus = true
       },
       handleTouchMove (e){
-        if(this.touchStatus){
-          const startY = this.$refs["A"][0].offsetTop;
-          const touchY = e.touches[0].clientY - startY;
-          const index = (touchY - startY) / 20;
-          if(index >= 0 && index < this.letters.length){
-            this.$emit('change', this.letters[index])
+        if (this.touchStatus) {
+          if (this.timer) {
+            clearTimeout(this.timer)
           }
+          // setTimeout()性能优化
+          this.timer = setTimeout(() => {
+            const touchY = e.touches[0].clientY - 79
+            const index = Math.floor((touchY - this.startY) / 20)
+            if (index >= 0 && index < this.letters.length) {
+              this.$emit('change', this.letters[index])
+            }
+          }, 16)
         }
       },
       handleTouchEnd (){
