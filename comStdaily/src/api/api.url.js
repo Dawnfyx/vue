b613e6c9f6a393a9d4ab8639f84f8185e3edf6c1
Dawn <15021677496@163.com>
2019-11-18 22:13:20 +0800

    del

--- comStdaily/src/api/api.url.js
+++
@@ -1,4 +0,0 @@
-export default {
-  development: '/api', //开发环境
-  production: '/' //生产环境
-}
--- comStdaily/src/axios/api.url.js
+++
@@ -1,8 +0,0 @@
-export default {
-  DOMAIN               : 'http://10.20.100.158',
-  baseURL              : 'http://localhost:8080/#/',
-  /* Util API */
-  util: {
-    image            : '/util/image.html',          // 图片上传
-  },
-};
